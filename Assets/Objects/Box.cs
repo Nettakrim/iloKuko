8a6b3b0ebe038ea479547721416b2c72c1350ed0
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-06-26 14:56:11 +0100

    added outlines, dragging objects now makes them rotate slightly

--- Assets/Objects/Box.cs
+++ Assets/Objects/Box.cs
@@ -12,6 +12,9 @@ public class Box : MonoBehaviour
     [SerializeField] private int maxOrder;
     private List<Object> previousHolds;
 
+    [SerializeField] private Vector3 heldScale;
+    [SerializeField] private float jostleAmount;
+
     void Start()
     {
         previousHolds = new List<Object>(maxOrder);
@@ -19,18 +22,25 @@ public class Box : MonoBehaviour
 
     void Update()
     {
-        Vector3 pos = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
 
         if (held)
         {
-            held.transform.position = dragOffset + pos;
-        } else if (Input.GetKeyDown(KeyCode.Mouse0))
+            held.transform.position = dragOffset + mousePos;
+
+            if (Input.GetKeyUp(KeyCode.Mouse0))
+            {
+                Jostle(mousePos, Vector3.one);
+                held = null;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            RaycastHit2D hit2D = Physics2D.Raycast(pos, Vector3.forward, 15);
+            RaycastHit2D hit2D = Physics2D.Raycast(mousePos, Vector3.forward, 15);
             if (hit2D)
             {
                 held = hit2D.collider.GetComponent<Object>();
-                dragOffset = hit2D.transform.position - pos;
+                dragOffset = hit2D.transform.position - mousePos;
 
                 previousHolds.Remove(held);
 
@@ -45,12 +55,24 @@ public class Box : MonoBehaviour
                 {
                     previousHolds[i].GetSpriteRenderer().sortingOrder = i + 1;
                 }
+
+                Jostle(mousePos, heldScale);
             }
         }
+    }
 
-        if (Input.GetKeyUp(KeyCode.Mouse0))
-        {
-            held = null;
-        }
+    public void Jostle(Vector3 mousePos, Vector3 scale)
+    {
+        Quaternion rotation = Quaternion.Euler(0, 0, Random.Range(-jostleAmount, jostleAmount));
+        dragOffset = rotation * new Vector3(dragOffset.x * scale.x / held.transform.localScale.x, dragOffset.y * scale.y / held.transform.localScale.y, dragOffset.z);
+
+        held.transform.localScale = scale;
+        held.transform.localRotation *= rotation;
+        held.transform.localPosition = dragOffset + mousePos;
+    }
+
+    void OnDisable()
+    {
+        held = null;
     }
 }
