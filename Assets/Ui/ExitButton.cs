98e54655942bc817f0ffcec756e33096c1ae84ab
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-06-29 19:51:25 +0100

    exit button now has circular click region

--- Assets/Ui/ExitButton.cs
+++ Assets/Ui/ExitButton.cs
@@ -6,9 +6,16 @@ public class ExitButton : FakeButton
 {
     [SerializeField] private float pressDown;
 
+    [SerializeField] private float radiusSquared;
+
     protected override void UpdateButton(int state)
     {
         base.UpdateButton(state);
         (transform as RectTransform).anchoredPosition = state == 2 ? new Vector2(0, -pressDown) : Vector2.zero;
+    }
+
+    protected override bool IsMouseOver()
+    {
+        return (Global.GetMousePos() - ((Vector2)transform.parent.position)).sqrMagnitude < radiusSquared;
     }
 }
